f6780029028efc55553f52580d75e1e993a4e9b6
camilo lesmes <c.lesmesl@uniandes.edu.co>
2023-09-02 22:35:15 -0500

    feat: Distribuidores gráficos

--- src/mi_aplicacion/gui/visor_materias.py
+++ src/mi_aplicacion/gui/visor_materias.py
@@ -14,6 +14,29 @@ class Aplicacion_Gui(QWidget):
         self.inicializar_GUI()
 
     def inicializar_GUI(self):
+        #inicializamos la ventana
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        #Creamos el distribuidor gráfico principal
+        self.distr_vertical = QVBoxLayout()
+
+        #Creamos la caja de materias
+        self.caja_materias = QGroupBox("Materia")
+        distr_caja_materias = QGridLayout()
+        self.caja_materias.setLayout(distr_caja_materias)
+
+        #Creamos la caja de botones
+        self.caja_botones = QGroupBox()
+        distr_caja_botones = QHBoxLayout()
+        self.caja_botones.setLayout(distr_caja_botones)
+        
+        #Agregamos las cajas a nuestra aplicación
+        self.distr_vertical.addWidget(self.caja_materias)
+        self.distr_vertical.addWidget(self.caja_botones)
+
+        #Definimos el distribuidor principal de la ventana.
+        self.setLayout(self.distr_vertical)
+
+        #Hacemos la ventana visible
         self.show()
