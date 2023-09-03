e8db399ba2bf900776ce0a0e046c76d06787364b
camilo lesmes <c.lesmesl@uniandes.edu.co>
2023-09-02 22:37:58 -0500

    feat: Etiquetas, campos de texto y botones

--- src/mi_aplicacion/gui/visor_materias.py
+++ src/mi_aplicacion/gui/visor_materias.py
@@ -26,16 +26,53 @@ class Aplicacion_Gui(QWidget):
         distr_caja_materias = QGridLayout()
         self.caja_materias.setLayout(distr_caja_materias)
 
+        #Creamos las etiquetas y campos de texto de la materia la caja de materias
+        self.etiqueta_nombre = QLabel('Nombre')
+        self.txt_nombre = QLineEdit()
+
+        # No es editable
+        self.txt_nombre.setReadOnly(True)
+
+        self.etiqueta_semestre = QLabel('Semestre')
+        self.txt_semestre = QLineEdit()
+
+        self.etiqueta_profesor = QLabel('Profesor')
+        self.txt_profesor = QLineEdit()
+
+        self.etiqueta_nota = QLabel('Nota')
+        self.txt_nota = QLineEdit()
+
+        #Agregamos a la caja de materias las etiquetas
+        distr_caja_materias.addWidget(self.etiqueta_nombre, 0,0)
+        distr_caja_materias.addWidget(self.etiqueta_semestre, 1,0)
+        distr_caja_materias.addWidget(self.etiqueta_profesor, 2,0)
+        distr_caja_materias.addWidget(self.etiqueta_nota, 3,0)
+
+        #Agregamos a la caja de materias los campos de texto
+        distr_caja_materias.addWidget(self.txt_nombre, 0,1)
+        distr_caja_materias.addWidget(self.txt_semestre, 1,1)
+        distr_caja_materias.addWidget(self.txt_profesor, 2,1)
+        distr_caja_materias.addWidget(self.txt_nota, 3,1)
+
         #Creamos la caja de botones
         self.caja_botones = QGroupBox()
         distr_caja_botones = QHBoxLayout()
         self.caja_botones.setLayout(distr_caja_botones)
         
+        #Creamos los botones para la caja de botones
+        self.boton_retroceder = QPushButton("<<")
+        self.boton_avanzar = QPushButton(">>")
+
+        #Agregamos los botones a la caja de botones
+        distr_caja_botones.addWidget(self.boton_retroceder)
+        distr_caja_botones.addWidget(self.boton_avanzar)
+        
+
         #Agregamos las cajas a nuestra aplicación
         self.distr_vertical.addWidget(self.caja_materias)
         self.distr_vertical.addWidget(self.caja_botones)
-
-        #Definimos el distribuidor principal de la ventana.
+        
+        #Definimos el distribuidor principal de la ventana             
         self.setLayout(self.distr_vertical)
 
         #Hacemos la ventana visible
